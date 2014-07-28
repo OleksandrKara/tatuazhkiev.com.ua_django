80ee2d31b5d4e115fa3cdc92362fdceea2e55aeb
Gennadiy Sobolev <gennadiy.sobolev@travisperkins.co.uk>
2014-07-28 18:56:23 +0300

    Time generating was added

--- tatuazhkiev/urls.py
+++ tatuazhkiev/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from tatuazhkiev.views import hello, my_homepage_view
+from tatuazhkiev.views import hello, my_homepage_view, current_datetime, hours_ahead
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,5 +11,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	('^hello/$', hello),
 	('^$', my_homepage_view),
+	('^date/$', current_datetime),
+	(r'^time/plus/(\d{1,2})/$', hours_ahead),
 	
 )
--- tatuazhkiev/views.py
+++ tatuazhkiev/views.py
@@ -1,7 +1,23 @@
 ﻿# -*- coding: utf-8 -*-
 from django.http import HttpResponse
+import datetime
 
 def hello(request):
     return HttpResponse("<h1>Здравствуй, Мир</h1>")
+	
 def my_homepage_view(request):
-	return HttpResponse("Главная страница")
+    return HttpResponse("Главная страница")
+	
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
