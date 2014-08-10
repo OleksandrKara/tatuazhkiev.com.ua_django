a5708cd66d3fb50ceea5678dc479dbf9cd184c2e
OleksandrKara <oleksandr_kara@epam.com>
2014-08-11 00:41:36 +0300

    Application(Fotos) was added, On PC mysql was installed and mysqld from web(where pip and setuptools)

--- tatuazhkiev/settings.py
+++ tatuazhkiev/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'tatuazhkiev.com.ua_django.books',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,12 @@ WSGI_APPLICATION = 'tatuazhkiev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+		'USER': 'root',
+		'PASSWORD': '1111',
+		'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
